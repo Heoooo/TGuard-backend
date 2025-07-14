0d2a2d374da7d0063ec8ab659259cf40fa069504
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-14 22:29:39 +0900

    feat: commit existing works

---
+++ src/main/java/com/tguard/tguard_backend/TguardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tguard.tguard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TguardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TguardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tguard/tguard_backend/TguardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tguard.tguard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TguardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
