86488ef00a78e600a2a8e74dd22c42234346c986
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-14 22:47:52 +0900

    feat: Rule 엔티티 생성

---
+++ src/main/java/com/tguard/tguard_backend/rule/Rule.java
@@ -0,0 +1,43 @@
+package com.tguard.tguard_backend.rule;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Rule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String ruleName;
+
+    @Column(nullable = false)
+    private String condition;
+
+    @Column(nullable = false)
+    private String value;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @Builder
+    private Rule(String ruleName, String condition, String value) {
+        this.ruleName = ruleName;
+        this.condition = condition;
+        this.value = value;
+    }
+}
