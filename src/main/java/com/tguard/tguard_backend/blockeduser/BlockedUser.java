dd60065d9b2e4e0e91b69425be36181d3155b1dc
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-20 17:48:22 +0900

    feat: BlockedUser 엔티티 생성

---
+++ src/main/java/com/tguard/tguard_backend/blockeduser/BlockedUser.java
@@ -0,0 +1,41 @@
+package com.tguard.tguard_backend.blockeduser;
+
+import com.tguard.tguard_backend.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BlockedUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String reason;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime blockedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.blockedAt = LocalDateTime.now();
+    }
+
+    @Builder
+    private BlockedUser(User user, String reason) {
+        this.user = user;
+        this.reason = reason;
+    }
+}
