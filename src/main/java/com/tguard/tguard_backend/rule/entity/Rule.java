422a8098cf0aa2c02908a8f5979d088fb946ccd6
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-20 19:37:53 +0900

    refactor: entity 패키지 경로 변경

--- src/main/java/com/tguard/tguard_backend/rule/entity/Rule.java
+++ src/main/java/com/tguard/tguard_backend/rule/entity/Rule.java
@@ -1,6 +1,5 @@
-package com.tguard.tguard_backend.blockeduser;
+package com.tguard.tguard_backend.rule.entity;
 
-import com.tguard.tguard_backend.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -12,30 +11,33 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class BlockedUser {
+public class Rule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(nullable = false)
+    private String ruleName;
 
     @Column(nullable = false)
-    private String reason;
+    private String condition;
+
+    @Column(nullable = false)
+    private String value;
 
     @Column(nullable = false, updatable = false)
-    private LocalDateTime blockedAt;
+    private LocalDateTime createdAt;
 
     @PrePersist
     public void prePersist() {
-        this.blockedAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now();
     }
 
     @Builder
-    private BlockedUser(User user, String reason) {
-        this.user = user;
-        this.reason = reason;
+    private Rule(String ruleName, String condition, String value) {
+        this.ruleName = ruleName;
+        this.condition = condition;
+        this.value = value;
     }
 }
