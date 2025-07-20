6d6332022b3bbac440b8e389f7166ff5fafe011d
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-20 19:34:52 +0900

    feat: UserRepository 레포지토리 생성

---
+++ src/main/java/com/tguard/tguard_backend/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tguard.tguard_backend.user.repository;
+
+import com.tguard.tguard_backend.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
