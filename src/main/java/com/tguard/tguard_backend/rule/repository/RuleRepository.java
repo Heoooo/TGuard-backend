5fa19034eefc8110c6646895f02e3db2e384bf1c
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-20 19:31:41 +0900

    feat: RuleRepository 레포지토리 생성

---
+++ src/main/java/com/tguard/tguard_backend/rule/repository/RuleRepository.java
@@ -0,0 +1,7 @@
+package com.tguard.tguard_backend.rule.repository;
+
+import com.tguard.tguard_backend.rule.entity.Rule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RuleRepository extends JpaRepository<Rule, Long> {
+}
