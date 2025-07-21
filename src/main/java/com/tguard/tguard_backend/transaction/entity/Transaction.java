7eae5f5ca197d4183e96777d71bb94aa30029424
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-21 22:25:48 +0900

    feat: channel 필드 및 updateStatus() 추가

--- src/main/java/com/tguard/tguard_backend/transaction/entity/Transaction.java
+++ src/main/java/com/tguard/tguard_backend/transaction/entity/Transaction.java
@@ -26,7 +26,13 @@ public class Transaction {
     private Double amount;
 
     private String location;
+
+    @Column(nullable = true)
     private String deviceInfo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Channel channel;
 
     @Column(nullable = false)
     private LocalDateTime transactionTime;
@@ -41,12 +47,17 @@ public class Transaction {
 
     @Builder
     private Transaction(User user, Double amount, String location, String deviceInfo,
-                        LocalDateTime transactionTime, Status status) {
+                        LocalDateTime transactionTime, Status status, Channel channel) {
         this.user = user;
         this.amount = amount;
         this.location = location;
         this.deviceInfo = deviceInfo;
         this.transactionTime = transactionTime;
+        this.status = status;
+        this.channel = channel;
+    }
+
+    public void updateStatus(Status status) {
         this.status = status;
     }
 }
