d73ab8c81a07a376f1ee60a17dabdb265ebaf177
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-21 22:26:07 +0900

    feat: channel 필드 추가

--- src/main/java/com/tguard/tguard_backend/transaction/dto/TransactionRequest.java
+++ src/main/java/com/tguard/tguard_backend/transaction/dto/TransactionRequest.java
@@ -1,10 +1,12 @@
 package com.tguard.tguard_backend.transaction.dto;
 
+import com.tguard.tguard_backend.transaction.entity.Channel;
 import jakarta.validation.constraints.NotNull;
 
 public record TransactionRequest(
         @NotNull Double amount,
         String location,
-        String deviceInfo
+        String deviceInfo,
+        @NotNull Channel channel
 ) {
 }
