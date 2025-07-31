ba8f45b47c1ad24682704471f47a4ca0c40eb903
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-07-31 16:47:05 +0900

    chore: kafka 환경 설정 추가

--- src/main/java/com/tguard/tguard_backend/TguardBackendApplication.java
+++ src/main/java/com/tguard/tguard_backend/TguardBackendApplication.java
@@ -2,8 +2,10 @@ package com.tguard.tguard_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class TguardBackendApplication {
 
 	public static void main(String[] args) {
