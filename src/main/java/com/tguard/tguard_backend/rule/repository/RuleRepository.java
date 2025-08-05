e24bec95800a152da7fe0ef4758821ca68e2ba29
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-08-05 17:45:30 +0900

    feat: existsByRuleName() 추가

--- src/main/java/com/tguard/tguard_backend/rule/repository/RuleRepository.java
+++ src/main/java/com/tguard/tguard_backend/rule/repository/RuleRepository.java
@@ -4,4 +4,5 @@ import com.tguard.tguard_backend.rule.entity.Rule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RuleRepository extends JpaRepository<Rule, Long> {
+    boolean existsByRuleName(String ruleName);
 }
