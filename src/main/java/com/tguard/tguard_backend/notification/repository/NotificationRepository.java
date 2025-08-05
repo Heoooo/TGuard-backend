234c1563bb717f710a27367d0931adfe4b0795bd
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-08-05 22:45:38 +0900

    feat: NotificationRepository Repository 생성

---
+++ src/main/java/com/tguard/tguard_backend/notification/repository/NotificationRepository.java
@@ -0,0 +1,11 @@
+package com.tguard.tguard_backend.notification.repository;
+
+import com.tguard.tguard_backend.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByTransaction_User_IdOrderByCreatedAtDesc(Long userId);
+}
+
