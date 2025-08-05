1330c0fb389aee2e3989d8f27182057fd8788143
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-08-05 17:44:33 +0900

    refactor: RuleRequest 상세설명 필드 추가 및 기존 필드 제거

--- src/main/java/com/tguard/tguard_backend/rule/dto/RuleRequest.java
+++ src/main/java/com/tguard/tguard_backend/rule/dto/RuleRequest.java
@@ -3,8 +3,6 @@ package com.tguard.tguard_backend.rule.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record RuleRequest(
-        @NotBlank String ruleName,
-        @NotBlank String condition,
-        @NotBlank String value
+        String description
 ) {
 }
