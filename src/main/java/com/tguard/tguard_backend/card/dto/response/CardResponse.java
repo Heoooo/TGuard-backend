a3fb21e0d5e09e3d4ef23aadad359eabae2f63f6
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-10-12 22:49:05 +0900

    refactor: CardResponse DTO 수정 (userId 제거, nickname 필드 추가)

--- src/main/java/com/tguard/tguard_backend/card/dto/response/CardResponse.java
+++ src/main/java/com/tguard/tguard_backend/card/dto/response/CardResponse.java
@@ -1,8 +1,5 @@
 package com.tguard.tguard_backend.card.dto.response;
 
 public record CardResponse(
-        Long id,
-        String brand,
-        String last4,
-        Long userId
+        Long id, String brand, String last4, String nickname
 ) {}
