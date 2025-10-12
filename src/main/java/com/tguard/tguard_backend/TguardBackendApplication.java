bb52fe060b16804726b649c34a21ec86cff7b61f
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-10-12 19:32:35 +0900

    feat: TguardBackendApplication 스케줄링 기능 활성화

--- src/main/java/com/tguard/tguard_backend/TguardBackendApplication.java
+++ src/main/java/com/tguard/tguard_backend/TguardBackendApplication.java
@@ -3,9 +3,11 @@ package com.tguard.tguard_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableKafka
+@EnableScheduling
 public class TguardBackendApplication {
 
 	public static void main(String[] args) {
