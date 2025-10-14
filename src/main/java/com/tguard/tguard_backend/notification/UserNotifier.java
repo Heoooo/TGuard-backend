634ec24e9fd045b3b81bcbb3f32fb0d2be04ec53
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-10-14 15:54:36 +0900

    refactor: UserNotifier 인터페이스 변경 (alertSummary 기반 notifyFraud 메서드 적용)

--- src/main/java/com/tguard/tguard_backend/notification/UserNotifier.java
+++ src/main/java/com/tguard/tguard_backend/notification/UserNotifier.java
@@ -3,5 +3,5 @@ package com.tguard.tguard_backend.notification;
 import com.tguard.tguard_backend.transaction.entity.Transaction;
 
 public interface UserNotifier {
-    void notifyFraud(Transaction transaction, String ruleName);
+    void notifyFraud(Transaction transaction, String alertSummary);
 }
