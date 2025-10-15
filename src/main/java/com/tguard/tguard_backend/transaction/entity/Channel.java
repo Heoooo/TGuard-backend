3e63ee8eefeebdb0ff125df33feb07f0f9172bec
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-10-15 14:53:07 +0900

    refactor: Channel enum 확장, 온라인/오프라인 채널 구분 추가

--- src/main/java/com/tguard/tguard_backend/transaction/entity/Channel.java
+++ src/main/java/com/tguard/tguard_backend/transaction/entity/Channel.java
@@ -1,5 +1,12 @@
 package com.tguard.tguard_backend.transaction.entity;
 
 public enum Channel {
-    MOBILE, POS, WEB, KIOSK, UNKNOWN
+
+        ONLINE,    // 웹/앱 기반 결제
+        OFFLINE,   // 오프라인 매장(POS, 키오스크)
+        MOBILE,    // 모바일 앱 전용 채널
+        WEB,       // 웹 브라우저 전용
+        POS,       // 전통적인 POS 단말
+        KIOSK,     // 키오스크 단말
+        UNKNOWN;   // 매핑 불가 시
 }
