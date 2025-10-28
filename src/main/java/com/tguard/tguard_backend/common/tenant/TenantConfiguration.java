c412343b03a7a86cc9c5b4be7ae2f7f417ec68e2
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-10-28 22:47:16 +0900

    refactor: TenantConfiguration 수정 및 TenantContextFilter 의존성 확장

--- src/main/java/com/tguard/tguard_backend/common/tenant/TenantConfiguration.java
+++ src/main/java/com/tguard/tguard_backend/common/tenant/TenantConfiguration.java
@@ -1,5 +1,7 @@
 package com.tguard.tguard_backend.common.tenant;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tguard.tguard_backend.tenant.service.TenantService;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +11,9 @@ import org.springframework.context.annotation.Configuration;
 public class TenantConfiguration {
 
     @Bean
-    public TenantContextFilter tenantContextFilter(TenantProperties tenantProperties) {
-        return new TenantContextFilter(tenantProperties);
+    public TenantContextFilter tenantContextFilter(TenantProperties tenantProperties,
+                                                   TenantService tenantService,
+                                                   ObjectMapper objectMapper) {
+        return new TenantContextFilter(tenantProperties, tenantService, objectMapper);
     }
 }
