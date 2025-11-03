78c52c667546ed58bcbf3c81a6b88375fb5560f6
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-11-03 13:51:30 +0900

    feat: UserRepository에 전화번호 기반 사용자 조회 메서드 추가

--- src/main/java/com/tguard/tguard_backend/user/repository/UserRepository.java
+++ src/main/java/com/tguard/tguard_backend/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.tguard.tguard_backend.user.repository;
 import com.tguard.tguard_backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByPhoneNumberAndTenantId(String phoneNumber, String tenantId);
     Optional<User> findByUsernameAndTenantId(String username, String tenantId);
     Optional<User> findByIdAndTenantId(Long id, String tenantId);
+    Optional<User> findByPhoneNumberAndTenantId(String phoneNumber, String tenantId);
+    List<User> findByTenantIdAndPhoneNumberIn(String tenantId, Collection<String> phoneNumbers);
 }
