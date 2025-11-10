2db29356c40aefab7e7c77bd398d8542d891b49a
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-11-10 15:42:14 +0900

    style: ErrorCode enum 주석 및 불필요 공백 제거

--- src/main/java/com/tguard/tguard_backend/common/exception/ErrorCode.java
+++ src/main/java/com/tguard/tguard_backend/common/exception/ErrorCode.java
@@ -6,22 +6,17 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    //인증, 인가
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
 
-    //사용자 관련
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 
-    //거래 관련
     TRANSACTION_NOT_FOUND(HttpStatus.NOT_FOUND, "거래 정보를 찾을 수 없습니다."),
     INVALID_TRANSACTION(HttpStatus.BAD_REQUEST, "유효하지 않은 거래입니다."),
 
-    //탐지 관련
     DETECTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이상 거래 탐지에 실패했습니다."),
     DETECTION_RESULT_NOT_FOUND(HttpStatus.NOT_FOUND, "탐지 결과를 찾을 수 없습니다."),
 
-    //공통
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력입니다.");
 
@@ -32,6 +27,4 @@ public enum ErrorCode {
         this.status = status;
         this.message = message;
     }
-
-
 }
