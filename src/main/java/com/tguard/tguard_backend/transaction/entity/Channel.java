b45217c3fb1cbaf6084f73e89f59bcfaf3f52fc2
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-11-11 16:24:00 +0900

    style: Channel enum 포맷 및 들여쓰기 정리

--- src/main/java/com/tguard/tguard_backend/transaction/entity/Channel.java
+++ src/main/java/com/tguard/tguard_backend/transaction/entity/Channel.java
@@ -2,11 +2,11 @@ package com.tguard.tguard_backend.transaction.entity;
 
 public enum Channel {
 
-        ONLINE,    // 웹/앱 기반 결제
-        OFFLINE,   // 오프라인 매장(POS, 키오스크)
-        MOBILE,    // 모바일 앱 전용 채널
-        WEB,       // 웹 브라우저 전용
-        POS,       // 전통적인 POS 단말
-        KIOSK,     // 키오스크 단말
-        UNKNOWN;   // 매핑 불가 시
+    ONLINE,
+    OFFLINE,
+    MOBILE,
+    WEB,
+    POS,
+    KIOSK,
+    UNKNOWN;
 }
