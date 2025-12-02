b8940738408909478410a1962e0f2210247261c3
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-12-02 13:43:43 +0900

    test: 테스트 환경에서 application context 로드하도록 프로필 설정

--- src/test/java/com/tguard/tguard_backend/TguardBackendApplicationTests.java
+++ src/test/java/com/tguard/tguard_backend/TguardBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.tguard.tguard_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TguardBackendApplicationTests {
 
 	@Test
